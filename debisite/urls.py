d5baccb044a4a701eb89df37fd4e1908e8025aa6
Sergey Titov <titovsb@gmail.com>
2021-12-07 01:03:16 +0300

    lesson01

--- debisite/urls.py
+++ debisite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp.views import indexpage, products, contact
 
 urlpatterns = [
+    path('', indexpage),
+    path('products/', products),
+    path('contact/', contact),
     path('admin/', admin.site.urls),
 ]
