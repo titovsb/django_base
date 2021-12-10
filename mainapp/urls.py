df5b06b7ac2a1b996f4eea52a8ec3a16680d75fa
Sergey Titov <titovsb@gmail.com>
2021-12-10 13:51:01 +0300

    начало. создали NS для mainapp

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -13,20 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
-# from mainapp.views import indexpage, products, contact
+from django.urls import path
 import mainapp.views as mainapp
 
+app_name = 'mainapp'    # обозначили имя приложения для указанного NS
+
 urlpatterns = [
-    # path('', mainapp.indexpage, name='index'),
-    # path('products/', mainapp.products, name='products'),
-    # path('contact/', mainapp.contact, name='contact'),
-    path('', include('mainapp.urls', namespace='primary')),
-    path('admin/', admin.site.urls),
+    path('', mainapp.indexpage, name='index'),
+    path('products/', mainapp.products, name='products'),
+    path('contact/', mainapp.contact, name='contact'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
